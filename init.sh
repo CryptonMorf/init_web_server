6827e60dd865c9a881c83078303d9e1acd272cc5
TNT <crypton.from@gmail.com>
2020-12-09 14:53:24 +0800

    install ghost through bash

--- init.sh
+++ init.sh
@@ -1,14 +1,36 @@
+# init ubuntu server with installing ghost cms
+# https://raw.githubusercontent.com/CryptonMorf/init_web_server/master/init.sh
 # !/bin/bash
 
 set -e
 
 apt update
 apt upgrade -y
-apt install nginx certbot python3-certbot-nginx
+apt install nginx certbot python3-certbot-nginx -y
+
+apt install mysql-server -y
+mysql <<< 'create user andy@localhost identified by passwd;grant all privileges on *.* to andy@localhost;flush privileges;'
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
+apt install nodejs -y
+
+npm install ghost-cli@latest -g
 
 ufw allow ssh
 ufw allow 'Nginx Full'
 ufw delete allow 'Nginx HTTP'
-ufw enable -y
+ufw enable <<< 'y'
 
 sysctl -w net.core.somaxconn=4096
+
+useradd andy
+passwd andy
+usermod -aG sudo andy
+su andy
+
+sudo mkdir -p /var/www/ghost
+sudo chown andy:andy /var/www/ghost
+sudo chmod 755 /var/www/ghost
+
+cd /var/www/ghost
+ghost install
